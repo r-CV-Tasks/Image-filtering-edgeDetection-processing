1f4a6659f9dd878296a5cadc2ccfdac98b7b6b05
Ahmad <ahmedmageed19@gmail.com>
2021-03-26 20:12:00 +0200

    Added Gaussian noise and edited Uniform noise
    Added the ability to choose certain values like SNR and implemented a seperate function for clipping

--- Filters/Noise.py
+++ Filters/Noise.py
@@ -6,11 +6,26 @@
 import numpy as np
 
 
-def UniformNoise(source: np.ndarray) -> np.ndarray:
+def clip(source: np.ndarray, high: [int, float], low: [int, float]) -> np.ndarray:
+    """
+        Clip Image in given Range
+    :param source: Input Image
+    :param high: Higher cut
+    :param low: Lower cut
+    :return: Clipped Image
+    """
+    out = np.copy(source)
+    out[out > high] = high
+    out[out < low] = low
+    return out
+
+
+def UniformNoise(source: np.ndarray, snr: float) -> np.ndarray:
     """
         Implementation of Uniform/Quantization Noise Filter
 
     :param source: Image to add Noise to
+    :param snr : Signal to Noise Ratio
     :return: Noisy Image
     """
     # Create Noise
@@ -18,22 +33,30 @@ def UniformNoise(source: np.ndarray) -> np.ndarray:
     src = np.copy(source)
 
     # Apply Uniform Noise Mask
-    out = src + noise
+    out = src*snr + noise*(1-snr)
 
     # CLipping Image in range 0, 255
-    out[out > 255] = 255
-    out[out < 0] = 0
+    out = clip(out, 255, 0)
     return out.astype(int)
 
 
-def GaussianNoise(source: np.ndarray) -> np.ndarray:
+def GaussianNoise(source: np.ndarray, sigma: [int, float], snr: float) -> np.ndarray:
     """
         Implementation of Gaussian Noise Filter
 
     :param source: Image to add Noise to
+    :param snr: Signal to Noise Ratio
+    :param sigma: Noise Variance
     :return: Noisy Image
     """
-    noise = np.random.normal(0, 1, size=source.shape)
+    noise = np.random.normal(0, sigma, size=source.shape)
     src = np.copy(source)
+    out = src*snr + noise * (1-snr)
 
-    return src + noise
+    # CLipping Image in range 0, 255
+    out = clip(out, 255, 0)
+    return out.astype(int)
+
+
+def SaltPepperNoise(source: np.ndarray, snr: float) ->np.ndarray:
+    pass
